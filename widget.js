b29ab7159c1f80e85b8add5e40dcca03e29a16fc
Justin <goyousalukis@gmail.com>
2016-07-27 19:31:07 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -137,7 +137,7 @@ cpdefine("inline:net-mydomain-widget-degreeindexer", ["chilipeppr_ready", /* oth
             this.btnSetup();
             this.createCanvas();
             this.forkSetup();
-            this.addT("currentPos", 0);
+            this.addT("currentPos", 10);
             //document.currentPos.value = "0";
             
             
